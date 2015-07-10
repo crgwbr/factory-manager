f3d9c939ce16d7fe08fe0faeaa135449dcc9524d
Craig Weber <crgwbr@gmail.com>
2015-07-10 14:25:45 -0400

    Add Vagrant VM for running app
    Adds a Vagrantfile and related provisioning playbook for running application.
    Uses nginx + unicorn stack.

---
+++ config.ru
@@ -0,0 +1,4 @@
+require 'rubygems'
+require_relative 'server/main.rb'
+
+run Sinatra::Application
---
+++ unicorn.rb
@@ -0,0 +1,7 @@
+working_directory '/var/www/factory-manager/current'
+pid '/var/www/factory-manager/current/tmp/pid/factory-manager.pid'
+stderr_path '/var/www/factory-manager/current/tmp/logs/factory-manager.error.log'
+stdout_path '/var/www/factory-manager/current/tmp/logs/factory-manager.log'
+listen '/var/www/factory-manager/current/tmp/sockets/unicorn.sock'
+worker_processes 2
+timeout 10
